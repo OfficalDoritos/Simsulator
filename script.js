2c44032f8d7ea1eb07487fa52526e4cfb2e01403
Jay <fatloser420@gmail.com>
2023-12-01 13:09:33 -0500

    icon and yike button added

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+/* 
+safety pop up
+disabled for development
+window.onbeforeunload = function () {
+  return "Data will be lost if you leave the page, are you sure?";
+};
+*/
+
 var hit = () => {
   var uh = $("<img>");
   uh.draggable();
   var lunk = $("#thisone").val();
   uh.attr("src", lunk);
   $("#box").append(uh);
+};
+
+var yike = () => {
+  var yike = $("<img src='../yike.jpeg'>");
+  yike.draggable();
+  $("#box").append(yike);
 };
